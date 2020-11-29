09dd58b6704cfbdce93490d0790ec045db52c060
fajarwz <fajarwindhuzulfikar@gmail.com>
2020-11-29 07:00:58 +0700

    add watermark

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,35 +1,22 @@
 <footer class="footer">
   <div class="container-fluid">
       <nav>
-          <ul class="footer-menu">
-              <li>
-                  <a href="#">
-                      Home
-                  </a>
-              </li>
-              <li>
-                  <a href="#">
-                      Company
-                  </a>
-              </li>
-              <li>
-                  <a href="#">
-                      Portfolio
-                  </a>
-              </li>
-              <li>
-                  <a href="#">
-                      Blog
-                  </a>
-              </li>
-          </ul>
+          
           <p class="copyright text-center">
-              ©
+              Theme: ©
               <script>
                   document.write(new Date().getFullYear())
               </script>
-              <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
+              <a href="http://www.creative-tim.com" target="_blank">Creative Tim</a>, made with love for a better web
+
+              &nbsp;|&nbsp;
+              App: IKET ©
+              <script>
+                  document.write('2020 - ' +new Date().getFullYear())
+              </script>
+              Made with <3 by <a href="http://fajarwz.medium.com" target="_blank">fajarwz</a>
           </p>
+
       </nav>
   </div>
 </footer>
--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -3,5 +3,7 @@
 <!-- CSS Files -->
 <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
 <link href="{{ asset('assets/css/light-bootstrap-dashboard.css?v=2.0.0 ') }}" rel="stylesheet" />
+{{-- custom css mix  --}}
+
 {{-- Custom CSS  --}}
 <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet" />
